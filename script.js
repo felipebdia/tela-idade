19cbb106ab86e3a5d0a52dd798b4499fe3a7e824
Felipe Badia <137180694+felipebdia@users.noreply.github.com>
2023-10-03 18:55:29 -0300

    idade

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
+function verificar() {
     var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas`
-    if (hora >= 0  && hora < 12) {
-        //BOM DIA
-        img.src = 'img/manha.png'
-        document.body.style.background = '#AC8C25'
-    } else if (hora >= 12 && hora < 18) {
-        //BOA TARDE
-        img.src = 'img/tarde.png'
-        document.body.style.background = '#D5D3D2'
-    } else {
-        // BOA NOITE
-        img.src = 'img/noite.png'
-        document.body.style.background = '#322944'
-    }
+    var ano = data.getFullYear()
+    var fAno = document.getElementById("txtano")
 }
